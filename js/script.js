c382d74e8bfd942f02e26d0c0f0f9038326538b4
Mehraj Hosen <meherajhosen786@gmail.com>
2025-03-25 05:16:56 +0600

    parralax add

--- js/script.js
+++ js/script.js
@@ -127,3 +127,40 @@ function edit(editButton) {
     });
   });
 }
+
+
+const parralax1 = document.querySelectorAll("#icons .img1");
+const parralaxArr1= Array.from(parralax1)
+let s = 250;
+
+parralaxArr1.map((item)=>{
+
+  document.body.addEventListener("mousemove", function (e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    console.log(x);
+  
+    item.style.transform = `
+      translate(
+        ${x / s}%,
+        ${y / s}%
+      )`;
+  });
+})
+const parralax2 = document.querySelectorAll("#icons .img2");
+const parralaxArr2= Array.from(parralax2)
+let ss = 250;
+
+parralaxArr2.map((item)=>{
+
+  document.body.addEventListener("mousemove", function (e) {
+    let x = e.clientX;
+    let y = e.clientY;
+  
+    item.style.transform = `
+      translate(
+        -${x / ss}%,
+        -${y / ss}%
+      )`;
+  });
+})
